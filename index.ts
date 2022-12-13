372f18f4eb218f2905d19fb7ab9231b8e27e389a
adpadillar <adpadillar25@gmail.com>
2022-12-13 12:18:16 -0700

    Modified index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
-import A_2022_1 from "./2022/1/a.js";
-import B_2022_1 from "./2022/1/b.js";
+// Use this to debug the problem you're working on
+// Example:
 
-console.log("Advent of code 2022 - Day 1");
-console.log("Part 1:");
-console.log(A_2022_1);
-console.log("Part 2:");
-console.log(B_2022_1);
+// import solution from "2022/1/a.js"
+// console.log(solution)
